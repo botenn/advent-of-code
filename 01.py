6fa1b5ba579392ca4b285f8535566e8cce9aa3af
josh coker <82056536+botenn@users.noreply.github.com>
2023-07-05 18:56:10 -0400

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,11 @@
+import os
+
+summed_groups = []
+with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
+    raw_groups = f.read().split("\n\n")
+    cleaned_groups = [group.split("\n") for group in raw_groups]
+    num_groups = [[int(i) for i in group if i != ""] for group in cleaned_groups]
+    for num_group in num_groups:
+        summed_groups.append(sum(int(i) for i in num_group))
+summed_groups.sort(reverse=True)
+print(summed_groups[0])
