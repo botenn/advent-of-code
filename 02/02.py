226ab98af121833145f40fc44054fa460d13ff8f
botenn <82056536+botenn@users.noreply.github.com>
2023-07-06 23:33:01 -0400

    day 2

---
+++ 02/02.py
@@ -0,0 +1,44 @@
+import os
+
+char_to_int = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+
+with open(os.path.join(os.path.dirname(__file__), "02-input.txt")) as f:
+    strategy_guide = f.read().split("\n")
+
+# Part 1
+
+score = 0
+
+for round in strategy_guide:
+    if round in ["A X", "B Y", "C Z"]:
+        score += 3
+        score += char_to_int[round[2]]
+    elif round in ["C X", "A Y", "B Z"]:
+        score += 6
+        score += char_to_int[round[2]]
+    elif round in ["A Z", "C Y", "B X"]:
+        score += char_to_int[round[2]]
+print("Part 1: " + str(score))
+
+# Part 2
+
+score = 0
+
+
+def get_losing_shape(counter_shape):
+    return {"A": "Z", "B": "X", "C": "Y"}.get(counter_shape)
+
+
+def get_winning_shape(counter_shape):
+    return {"A": "Y", "B": "Z", "C": "X"}.get(counter_shape)
+
+
+for round in strategy_guide:
+    if round[2] == "X":
+        score += char_to_int[get_losing_shape(round[0])]
+    elif round[2] == "Y":
+        score += char_to_int[round[0]] + 3
+    elif round[2] == "Z":
+        score += char_to_int[get_winning_shape(round[0])] + 6
+
+print("Part 2: " + str(score))
